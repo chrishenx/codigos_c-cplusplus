a43c32019eb0ef4f5302e11aed600c6714182df7
chrishenx <chrishendrix24@gmail.com>
2014-11-08 21:56:35 -0600

    Programa "Hola mundo"
    Este es el primer programa que todo mundo realiza cuando comienza a
    aprender cualquier lenguaje

---
+++ Hola mundo.cpp
@@ -0,0 +1,16 @@
+/*
+ * Programa "Hola mundo"
+ * @author CPlayMasH
+ ** Contenido puramente educativo
+ */
+
+#include <iostream> // Biblioteca para entrada y salida estandar
+	
+using namespace std; // En el espacio de nombres "std" se encuentra definido "cout"
+
+int main() {
+	cout << "Hola mundo!" << endl; // Mandamos la cadena al flujo de salida "cout"
+	cout << "Aprendiendo C++... Yeah" << endl; // Tambien en "std" esta definido "endl" -> "Fin de linea" 
+	
+	return 0; // El programa retorna un valor, en este caso 0.
+}
