e2e7d9e311bffe3eed0d966d73762c179edccbf3
chrishenx <chrishendrix24@gmail.com>
2014-11-21 15:26:42 -0600

    Nuevos códigos ejemplo.

---
+++ Entrada y salida.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // Cabecera para entrada y salida estandar
+
+using namespace std;
+
+int main() {
+    char caracter;
+    int a, b;
+    double flotante;
+    string cadena;
+
+    cin >> caracter;
+    cout << caracter << endl;
+    cin >> a >> b;
+    cout << a << ' ' << b << endl;
+
+    cin >> flotante;
+    cout << flotante << endl;
+    cin >> cadena;
+    cout << cadena << endl;
+
+    return 0;
+}
---
+++ Tipos de dato.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // En realidad no usamos esta biblioteca,
+					// solo la incluyo por costumbre.
+
+using namespace std;
+
+int main() {
+	int si = 1; //Variable "signed int" con un valor inicial de 1
+	unsigned int ui; //Variable "unsigned int" sin valor inicial 
+	char c = 'K'; //Variable "char" que almacena un caracter
+	float f = 4.1; //Variable flotante de 4 bytes
+	double d = 1e-5; //Variable flotante de doble presicion inicializada
+					 //con notacion exponencia { 1X10^(-5) }
+	short s = -10;//Variable entera de 2 bytes con signo
+	unsigned long long ll = 20000; //Variable entera de 8 bytes sin signo
+    return 0;
+}
