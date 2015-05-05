09f7109a8274c28420406c1d5186456797b86e21
Christian González <chrishendrix24@gmail.com>
2015-05-04 20:42:45 -0500

    Operaciones básicas en C
    Código ejemplo que muestra una calculadora básica en C

---
+++ operaciones_calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  float a, b;
+
+  printf("Programa que realiza las 4 operaciones basicas entre dos numeros\n\n");
+  printf("Introduce el primer numero (a): ");
+  scanf("%f", &a);
+  printf("Introduce el segundo numero (b): ");
+  scanf("%f", &b);
+
+  printf("\n");
+  printf("a + b = %f\n", a + b);
+  printf("a - b = %f\n", a - b);
+  printf("b - a = %f\n", b - a);
+  printf("a * b = %f\n", a * b);
+  printf("a / b = ");
+  if (b != 0) {
+    printf("%f", a / b);
+  } else {
+    printf("indefinido");
+  }
+  printf("\nb / a = ");
+  if (a != 0) {
+    printf("%f", b / a);
+  } else {
+    printf("indefinido");
+  }
+  printf("\n");
+
+  printf("\nEl programa ha terminado!\n\n");
+  system("pause");
+
+  return 0;
+}
